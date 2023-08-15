a77bd3168e0e27df7c9adc031ac8a18542e4cca1
WEIV2510 <ratthakarn.ma.65@ubu.ac.th>
2023-08-15 22:32:15 +0700

    dc

--- src/App.js
+++ src/App.js
@@ -1,49 +1,49 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Container, Form, Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 
 function App() {
   const { register, handleSubmit } = useForm();
-  const { data, setData } = useState();
+  const [data, setData] = useState();
   return (
     <Container>
-      <h4>React Hook From</h4>
-      <Form onSubmit = {handleSubmit((data)=> setData(JSON.stringify(data)))}
+      <h4>React Hook Form</h4>
+      <Form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
         <Form.Group className="mb-3">
-          <Form.Label>Email Address</Form.Label>
+          <Form.Label>Email address</Form.Label>
           <Form.Control
             {...register("email")}
             type="email"
             placeholder="Enter email"
           ></Form.Control>
-       </Form.Group>
+        </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Password</Form.Label>
           <Form.Control
             {...register("password")}
             type="password"
-            placeholder="Enter passwprd"
+            placeholder="Enter password"
           />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>ประเภทสมาชิก</Form.Label>
-          <Form.Select {...register("userType")}>
+          <Form.Select {...register("usertType")}>
             <option value="1" selected>
               ทั่วไป
             </option>
-            <option value="2">VIP</option>
-            <option value="3">Admin</option>
+            <option value="2">vip</option>
+            <option value="3">ผู้ดูแลระบบ</option>
           </Form.Select>
         </Form.Group>
-        <Form.Group className="mb-3">
+        <Form.Group>
           <Form.Check
             {...register("remember")}
             type="checkbox"
             label="Remember me"
-          ></Form.Check>
+          />
         </Form.Group>
-        <Form.Group className="='mb-3">
+        <Form.Group className="mb-3">
           <Form.Text>{data}</Form.Text>
         </Form.Group>
         <Button type="submit">Submit</Button>
